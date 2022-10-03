c8a7233b7e195242c80e27018d1dbd777eada80b
Benjamin Klarer <benjaminklarer@gmail.com>
2022-10-02 20:46:22 -0700

    commit tree 445c850dd4d943c92d4c5e96068bb60a55640b5e

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
 # Write your code here.
+
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(speakers)
+    badge_array = []
+    speakers.each do |name|
+        badge_string = "Hello, my name is #{name}."
+        badge_array.push(badge_string)
+    end
+    return badge_array
+end
+
+def assign_rooms(speakers)
+    speakers.map.with_index(1) do |speaker, room_number|
+    "Hello, #{speaker}! You'll be assigned to room #{room_number}!"
+    end
+end
+
+def printer(speakers)
+    batch_badge_creator(speakers).each do |badge|
+        puts badge
+    end
+    assign_rooms(speakers).each do |room_string|
+        puts room_string
+    end
+end
